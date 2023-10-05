bc3b02479e2cfc6947ca627a5baefa93e5b2c398
ygo <ka.ra.si.naporitan@gmail.com>
2023-10-05 14:12:30 +0900

    websocket通信の例

--- test/websockettest.py
+++ test/websockettest.py
@@ -1,13 +1,18 @@
 import asyncio
-import websockets, ssl
+import websockets
 
-async def hello():
-    uri = "ws://fastsample-389906.an.r.appspot.com/ws"
-    async with websockets.connect(
-        uri=uri
-    ) as websocket:
-        await websocket.send("Hello world!")
-        print(await websocket.recv())
+async def connect_to_websocket():
+    uri = "ws://fastsample-389906.an.r.appspot.com/ws"  # FastAPIサーバーのWebSocketエンドポイントのURIを指定します
+
+    async with websockets.connect(uri) as websocket:
+        try:
+            # 接続が確立されたらサーバーからのメッセージを待機
+            while True:
+                message = await websocket.recv()
+                print(f"Received message from server: {message}")
+        except websockets.exceptions.ConnectionClosed:
+            print("Connection to the server is closed.")
 
 if __name__ == "__main__":
-    asyncio.run(hello())
+    # 非同期イベントループを作成してWebSocketに接続
+    asyncio.get_event_loop().run_until_complete(connect_to_websocket())
