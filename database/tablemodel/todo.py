f5e3bc5b070c1f243870df6b7ff5e3acc4e1216a
ygo <ka.ra.si.naporitan@gmail.com>
2023-10-29 15:21:24 +0900

    todo作成

--- database/tablemodel/todo.py
+++ database/tablemodel/todo.py
@@ -1,7 +1,20 @@
+# $ python todo.py でテーブル作成
 from sqlalchemy import Boolean, Column, Integer, String, create_engine
+from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///./test.db"
+# SQLALCHEMY_DATABASE_URI = "postgresql://user:password@postgresserver/db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URI,
+    connect_args={"check_same_thread": False},
+    echo=True
+)
+
 Base = declarative_base()
+
 # Todoテーブルの定義
 class Todo(Base):
     __tablename__ = 'todos'
@@ -9,39 +22,8 @@ class Todo(Base):
     title = Column('title', String(200))
     done = Column('done', Boolean, default=False)
 
-import aiopg.sa
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+# テーブル作成
+Base.metadata.create_all(bind=engine)
 
-async def connect_to_postgresql():
-    conn = await aiopg.connect(
-        host='localhost',
-        port=5432,
-        user='your_username',
-        password='your_password',
-        database='your_database'
-    )
-    return conn
-
-async def create_table(engine):
-    metadata = MetaData()
-    your_table = Table(
-        'your_table', metadata,
-        Column('id', Integer, primary_key=True),
-        Column('name', String),
-        ...
-    )
-    await metadata.create_all(engine)
-
-async def setup():
-    connection = await connect_to_postgresql()
-    engine = create_engine(connection)
-    await create_table(engine)
-    return connection, engine
-
-from sqlalchemy.sql import select
-
-async def execute_query(engine):
-    async with engine.begin() as conn:
-        result = await conn.execute(select([your_table]))
-        rows = await result.fetchall()
-        return rows
+# DB接続用のセッションクラス インスタンスが作成されると接続する
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
