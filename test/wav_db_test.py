d031482c8b65f68c28cdb7b140c5f5925f33f2ca
ygo <ka.ra.si.naporitan@gmail.com>
2023-11-02 18:17:28 +0900

    dbからwavへの書き込み成功

--- test/wav_db_test.py
+++ test/wav_db_test.py
@@ -2,15 +2,18 @@ import wave
 import librosa
 import numpy as np
 from datetime import datetime, timedelta
+import struct
 
 from sqlalchemy import (
     Boolean,
+    BigInteger,
     Column,
     Integer,
     String,
     TIMESTAMP,
     DECIMAL,
     BOOLEAN,
+    BINARY,
     Float,
     create_engine
 )
@@ -18,6 +21,7 @@ from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 FileName = './data/audio_2/log_20230321_124636.wav' # mono
+FileName = './data/toujyo.wav' # stereo
 SQLALCHEMY_DATABASE_URI = "sqlite:///./sqlite/test.db"
 
 Base = declarative_base()
@@ -29,13 +33,14 @@ ENGINE = create_engine(
 )
 
 class WaveTable(Base):
-    __tablename__ = 'wavetable'
+    __tablename__ = 'wavetable2'
     time = Column('time', TIMESTAMP, primary_key=True)
     sampling_freq = Column('sampling_freq', DECIMAL)
     channel = Column('channel', Integer)
     sample_width = Column('sample_width', Integer)
     frame_rate = Column('frame_rate', Integer)
-    frame_count = Column('frame_count', Float)
+    frame_count_float = Column('frame_count_float', Float)
+    frame_count = Column('frame_count', BINARY)
 
 def wav_create_db(filename:str):
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=ENGINE)
@@ -47,14 +52,14 @@ def wav_create_db(filename:str):
     width = wf.getsampwidth()
     sampling_rate = wf.getframerate()
     frames = wf.getnframes()
+    # waveの実データを取得し、数値化
+    data = wf.readframes(wf.getnframes())
+    X:np.ndarray[np.int16] = np.frombuffer(data, dtype=np.int16)
     # wavファイルの音声データを読み込む
     y, sr = librosa.load(path=filename, sr=sampling_rate)
     time_rate = 1.0 / sr
 
-    for i,rate in enumerate(y):
-        if i % sr == 0:
-            pass
-            #print(rate)
+    for rate,float_rate in zip(X,y):
         time_pkey += timedelta(microseconds=time_rate*1000000)
         session.add(WaveTable(
             time=time_pkey,
@@ -62,6 +67,7 @@ def wav_create_db(filename:str):
             channel=channels,
             sample_width=width,
             frame_rate=sampling_rate,
+            frame_count_float=float_rate,
             frame_count=rate
         ))
 
@@ -88,19 +94,54 @@ def wav_read_db(before_time:datetime, after_time:datetime):
             after_time
         )
     ).all()
+
+    byte = b''
     for data in wav_data:
-        print(data.time)
-        print(data.sampling_freq)
-        print(data.channel)
-        print(data.sample_width)
-        print(data.frame_rate)
-        print(data.frame_count)
+        byte += data.frame_count
+
+    # 90秒分に相当するフレーム数を算出
+    time = 1
+    frames = int(wav_data[0].channel * wav_data[0].sampling_freq * time)
+    X = np.frombuffer(byte, dtype=np.int16)
+
+    # 出力データを生成
+    outf = './data/test.wav'
+    Y = X[:frames]
+    print(type(Y), len(Y))
+    outd = struct.pack("h" * len(Y), *Y)
+
+    # 書き出し
+    ww = wave.open(outf, 'w')
+    ww.setnchannels(wav_data[0].channel)
+    ww.setsampwidth(wav_data[0].sample_width)
+    ww.setframerate(wav_data[0].sampling_freq)
+    ww.writeframes(outd)
+    ww.close()
 
 def table_in():
     # テーブル作成
     Base.metadata.create_all(bind=ENGINE)
 
+def float2binary(data:np.float32, sampwidth:int) -> bytes:
+    data = (data*(2**(8*sampwidth-1)-1)).reshape(data.size, 1) # Normalize (float to int)
+    frames:bytes = b''
+    if sampwidth == 1:
+        data = data + 128
+        frames = data.astype(np.uint8).tobytes()
+    elif sampwidth == 2:
+        frames = data.astype(np.int16).tobytes()
+    elif sampwidth == 3:
+        a32 = np.asarray(data, dtype = np.int32)
+        a8 = (a32.reshape(a32.shape + (1,)) >> np.array([0, 8, 16])) & 255
+        frames = a8.astype(np.uint8).tobytes()
+    elif sampwidth == 4:
+        frames = data.astype(np.int32).tobytes()
+    return frames
+
+
 if __name__ == '__main__':
-    table_in()
+    #table_in()
     #wav_create_db(FileName)
-    wav_read_db(datetime(2023, 11, 2, 10, 43, 52, 8), datetime(2023, 11, 2, 10, 43, 53, 8))
+    #wav_read_db(datetime(2023, 11, 2, 17, 59, 32, 154365), datetime(2023, 11, 2, 17, 59, 34, 154366))
+    wav_read_db(datetime(2023, 11, 2, 18, 14, 25, 989078), datetime(2023, 11, 2, 18, 14, 28, 989079))
+    #wavreadtest()
