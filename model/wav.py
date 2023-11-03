53cf40bc02ef340844890c8f97563557e63401cb
ygo <ka.ra.si.naporitan@gmail.com>
2023-11-03 15:40:41 +0900

    wavファイルのモデル追加

---
+++ model/wav.py
@@ -0,0 +1,55 @@
+from typing import List
+import numpy as np
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    TIMESTAMP,
+    DECIMAL,
+    BINARY,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from pydantic import BaseModel
+from datetime import datetime
+
+Base = declarative_base()
+
+class WaveFileTable(Base):
+    __tablename__ = 'wavefile'
+    filename = Column('filename', String(200), primary_key=True)
+    sampling_freq = Column('sampling_freq', DECIMAL)
+    channel = Column('channel', Integer)
+    sample_width = Column('sample_width', Integer)
+    start_time = Column('start_time', TIMESTAMP)
+    end_time = Column('end_time', TIMESTAMP)
+
+class WaveTable(Base):
+    __tablename__ = 'wavetable'
+    time = Column('time', TIMESTAMP, primary_key=True)
+    frame_count = Column('frame_count', BINARY)
+
+class WaveFile(BaseModel):
+    filename: str
+    sampling_freq: float
+    channel: int
+    sample_width: int
+    start_time: datetime
+    end_time: datetime
+
+    class Config:
+        orm_mode = True
+
+class Wave(BaseModel):
+    time: datetime
+    frame_count: List[np.int16]
+
+    class Config:
+        orm_mode = True
+
+class ReadWaveFile(BaseModel):
+    filename: str
+    sampling_freq: float
+    channel: int
+    sample_width: int
+    create_time: datetime
+    wav_buffer16: np.ndarray[np.int16]
