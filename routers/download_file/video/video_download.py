78fc512d64a773f884fbc8422f7e83e5b43f85f8
ygo <ka.ra.si.naporitan@gmail.com>
2023-11-15 15:53:49 +0900

    s

--- routers/download_file/video/video_download.py
+++ routers/download_file/video/video_download.py
@@ -21,7 +21,7 @@ env = EnvConfig()
 router = APIRouter()
 
 """
-http://localhost:5000/download-file/pic/?start_time=2023-10-23%2010:59:00&end_time=2023-10-25%2011:01:00
+http://localhost:5000/download-file/video/?start_time=2023-10-23%2010:59:00&end_time=2023-10-25%2011:01:00
 """
 @router.get("/download-file/video/")
 async def download_file_tmp(
