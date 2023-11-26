6d72e0897d92befe1ed3570ce3046c16aba1815e
ygo <ka.ra.si.naporitan@gmail.com>
2023-11-26 15:33:26 +0900

    wavだけを保存するエンドポイントを追加

--- routers/save_file/wav/wav_save.py
+++ routers/save_file/wav/wav_save.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, File, UploadFile, Form, Depends
 from sqlalchemy.orm import Session
 import numpy as np
 
+import re
 import os
 import shutil
 from pathlib import Path
@@ -11,7 +12,13 @@ from datetime import datetime, timedelta
 from controllers.wav.wav_read import async_wav_read
 from model.wav import WaveFileTable,WaveTable
 
+from packages.file_time.creation_time import creation_date
 from packages.db.database import get_db
+from packages.gcs.gcs import GCSWrapper
+
+from model.envconfig import EnvConfig
+
+env = EnvConfig()
 
 router = APIRouter()
 """
@@ -31,6 +38,58 @@ $ curl -X POST
     -F "fileb=@archive.zip;type=application/x-zip-compressed"
 """
 @router.post("/save-upload-file/wav/")
+async def save_upload_file_tmp(
+    fileb: UploadFile=File(...),
+    #token:str=Form(...),
+    db: Session = Depends(get_db)
+):
+    tmp_path:Path = ""
+    gcs_path = os.path.basename(fileb.filename)
+    GCS = GCSWrapper(bucket_id=env.BUCKET_NAME)
+    try:
+        print(type(fileb))# <class 'starlette.datastructures.UploadFile'>
+        print(type(fileb.file)) #<class 'tempfile.SpooledTemporaryFile'>
+        suffix = Path(fileb.filename).suffix
+        print(fileb.filename)
+        with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+            shutil.copyfileobj(fileb.file, tmp)
+            tmp_path = Path(tmp.name)
+            print(tmp_path)
+
+        GCS.upload_file(
+            local_path=tmp_path.as_posix(),
+            gcs_path=gcs_path
+        )
+
+        wav_file = await async_wav_read(
+            filepath=tmp_path.as_posix(),
+            filename=os.path.basename(fileb.filename)
+        )
+
+        db.add(WaveFileTable(
+            filename=wav_file.filename,
+            sampling_freq=wav_file.sampling_freq,
+            channel=wav_file.channel,
+            sample_width=wav_file.sample_width,
+            start_time=wav_file.create_time,
+            end_time=wav_file.create_time + timedelta(microseconds=1.0/wav_file.sampling_freq*wav_file.frames*1000000)
+        ))
+
+        print(wav_file.create_time)
+
+        db.commit()
+    finally:
+        fileb.file.close()
+    return {
+        "filename": fileb.filename,
+        "temporary_filepath": tmp_path,
+        "start_time": wav_file.create_time,
+        "end_time": wav_file.create_time + timedelta(microseconds=1.0/wav_file.sampling_freq*wav_file.frames*1000000),
+        #"token": token,
+        "fileb_content_type": fileb.content_type,
+    }
+
+@router.post("/save-upload-file/wav-timestamp/")
 async def save_upload_file_tmp(
     fileb: UploadFile=File(...),
     #token:str=Form(...),
