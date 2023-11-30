a235a2279c0721da358e6a7e8432b3b49851c26b
ygo <ka.ra.si.naporitan@gmail.com>
2023-11-30 11:36:36 +0900

    テーブル定義更新

--- model/csv.py
+++ model/csv.py
@@ -15,6 +15,12 @@ from packages.db.database import Base as DBBase
 
 class CSVFileTable(DBBase):
     __tablename__ = 'csvfile'
+    filename = Column('filename', String(200), primary_key=True)
+    create_time = Column('create_time', TIMESTAMP)
+    bucket_name = Column('bucket_name', String(200))
+
+class CSVTable(DBBase):
+    __tablename__ = 'csvtable'
     time = Column('time', TIMESTAMP, primary_key=True)
     raw_data = Column('raw_data', Integer)
     flag = Column('flag', Integer)
