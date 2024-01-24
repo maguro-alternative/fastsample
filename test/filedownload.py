768bd95611b5292d86cb29498212651962c1da17
ygo <ka.ra.si.naporitan@gmail.com>
2024-01-24 17:00:20 +0900

    tuk

---
+++ test/filedownload.py
@@ -0,0 +1,10 @@
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    print(200)
+    # python filedownload.py pic 2023-10-25-5:59:00 2023-10-25-6:01:00
+    # python filedownload.py video 2023-10-23-10:59:00 2023-10-25-11:01:00
+    # python filedownload.py wav 2023-10-23-10:59:00 2023-10-25-11:01:00
+    # python filedownload.py csv 2023-10-23-10:59:00 2023-10-25-11:01:00
+    print("download file: ")
